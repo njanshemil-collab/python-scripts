db5b920c7c4efa2afc484fac786c03329228a3d4
Shemil <njanshemil@gmail.com>
2026-04-29 15:50:38 +0530

    Add files via upload

--- file_organizer.py
+++ file_organizer.py
@@ -4,5 +4,6 @@ user=input("enter : ")
 file=os.listdir(user)
 for i in file:
     split=os.path.splitext(i)
-    os.makedirs(os.path.join(user,split[1]),exist_ok=True)
-    os.rename(os.path.join(user,i),os.path.join(os.path.join(user,split[1]),i))
+    x=split[1][1:]
+    os.makedirs(os.path.join(user,x),exist_ok=True)
+    os.rename(os.path.join(user,i),os.path.join(os.path.join(user,x),i))
