d66f08153fe9e043b2ac4367d958787a792391d3
Shemil <njanshemil@gmail.com>
2026-05-01 23:32:41 +0530

    Add files via upload

--- excel_report_maker.py
+++ excel_report_maker.py
@@ -1,11 +1,19 @@
+import os
+print(os.getcwd())
 import pandas as pd
-import openpyxl
 print("enter your csv file ")
-pat=input("enter :")
-df=pd.read_csv(pat)
+while True:
+ pat=input("enter valid path :")
+ try:
+  df=pd.read_csv(pat)
+  break
+ except FileNotFoundError:
+  print("give valid csv path ")
 flnme=input("enter file name :")
 print("enter your sheet name ")
 sht=input("name :")
 df.to_excel(flnme+".xlsx",index=False,sheet_name=sht)
 print("file name : ",flnme+".xlsx")
 print("sheet name : ",sht)
+print("rows = ",df.shape[0])
+print("colomns = ",df.shape[1])
