346564a51c23378e7d55054ebc35822eaf3aafb6
Shemil <njanshemil@gmail.com>
2026-05-05 20:12:44 +0530

    Add files via upload

--- price_scraper.py
+++ price_scraper.py
@@ -1,9 +1,24 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests as rq
 fet=rq.get("https://books.toscrape.com")
+fet.encoding="utf-8"
 soup=BeautifulSoup(fet.text,"html.parser")
 print(soup.title.text)
 lis=soup.find_all("h3")
 price=soup.find_all("p",class_="price_color")
 for i,j in zip(lis,price):
-    print(i.a["title"],j.text)
+    print(i.a["title"]," - ",j.text)
+cleanlis=[]
+for titl in lis:
+    b=titl.a["title"]
+    cleanlis.append(b)
+cleanprice=[]
+for rte in price:
+    c=rte.text
+    cleanprice.append(c)   
+books={}
+books["book_title"]=cleanlis
+books["price"]=cleanprice
+df=pd.DataFrame(books)
+df.to_csv(r'C:\Users\Lenovo\OneDrive\Desktop\DODOW\books.csv')
